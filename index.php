6e6b135e928c7b1a7b886094744c9a5839410639
Aluno <Aluno@INFO02>
2019-08-06 18:38:34 -0300

    atzinha

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
+    </head>
+    <body>
+        <nav>
+            <div class="conatainer">
+                <div class="jumbotron" >
+                    <h1 align="center">Reserva Aí</h1>
+                </div>
+            </div>
+        </nav>
+        
+        
+        
+        <div class="container">
+    <div class="row">
+      <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+        <div class="card card-signin my-5">
+          <div class="card-body">
+            <h5 class="card-title text-center">ENTRAR</h5>
+            <form class="form-signin">
+              <div class="form-label-group">
+                <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+                <label for="inputEmail">Username</label>
+              </div>
+
+              <div class="form-label-group">
+                <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+                <label for="inputPassword">Password</label>
+              </div>
+
+              <div class="custom-control custom-checkbox mb-3">
+                <input type="checkbox" class="custom-control-input" id="customCheck1">
+                <label class="custom-control-label" for="customCheck1">Esqueceu a senha?</label>
+               
+              </div>
+              <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Entrar</button>
+              <hr class="my-4">
+              
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+    </body>
+</html>
